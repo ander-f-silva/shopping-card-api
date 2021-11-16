451f7f479d0e89421dbd0baa1a9e9b895b5608c6
andferreira <anderson.ferreira@mercadolivre.com>
2021-11-16 15:23:36 -0300

    feat: Add an factory to load the data of file product json

---
+++ src/main/java/io/sc/checkout/infrastructure/database/StorageMemory.java
@@ -0,0 +1,40 @@
+package io.sc.checkout.infrastructure.database;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.micronaut.context.annotation.Factory;
+import io.sc.checkout.business.dto.Product;
+import jakarta.inject.Singleton;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Factory
+public class StorageMemory {
+
+    @Singleton
+    List<Product> products() throws IOException {
+        //TODO: The path is in env path
+        try (InputStream is = TypeReference.class.getResourceAsStream("/json/products.json")) {
+            String jsonText = new BufferedReader(
+                    new InputStreamReader(is, StandardCharsets.UTF_8))
+                    .lines()
+                    .collect(Collectors.joining("\n"));
+
+            return Arrays.asList(new ObjectMapper().readValue(jsonText, Product[].class));
+        } catch (JsonProcessingException e) {
+            //TODO: Add an logger with error
+            e.printStackTrace();
+        }
+
+        //TODO: Adjusted message
+        throw new IOException("Error when load the file of products.json");
+    }
+}
